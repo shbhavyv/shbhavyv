842994ffcbb920c9cb619d99cd5c6642537dbf4e
shbhavyv <shbhavyv@sheridancollege.ca>
2024-09-18 14:34:26 -0400

    second comment1

---
+++ src/main/java/com/mycompany/myfirstproject1/MyFirstProject1.java
@@ -0,0 +1,17 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.myfirstproject1;
+
+/**
+ *
+ * @author shahb
+ */
+// monday demonstration
+public class MyFirstProject1 {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
